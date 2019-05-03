1c6512168038b5364c3f5886f949e05a43e106c0
Drake <drake535@hotmail.com>
2019-05-03 18:59:30 +0200

    Update AssemblyInfo

--- MpqLib/Properties/AssemblyInfo.cs
+++ MpqLib/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MpqLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription( "Library for reading and building MPQ files" )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MpqLib")]
-[assembly: AssemblyCopyright("Copyright ©  2006")]
+[assembly: AssemblyCopyright( "Copyright © 2006-2019" )]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "1.1.*" )]
 [assembly: AssemblyFileVersion("1.0.0.0")]
